0925648bffea306f1992cf1f1483da88b94768f2
Johannes Förstner <johannes@famfoerstner.de>
2026-09-19 18:38:44 +0200

    Build demo map always

--- build.bat
+++ build.bat
@@ -21,5 +21,11 @@ robocopy "%bits%\world\global\moods" "%tmp%\Bits\world\global\moods" /E
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
+:: Compile demo map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\maps\%mod%-demo" "%tmp%\Bits\world\maps\%mod%-demo" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%-demo.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
 :: Cleanup
 rmdir /S /Q "%tmp%\Bits"
--- run-demo.bat
+++ run-demo.bat
@@ -6,14 +6,6 @@ set ds=%DungeonSiege%
 :: Compile resource file
 call "%bits%\build.bat" %*
 
-:: Compile demo map file
-set copyright=CC-BY-SA 2026
-set author=Johannes Förstner
-rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\world\maps\autumn-demo" "%tmp%\Bits\world\maps\autumn-demo" /E
-"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\An Autumn Mod-demo.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
-if %errorlevel% neq 0 pause
-
 ::pause
 
 :: Run it!
